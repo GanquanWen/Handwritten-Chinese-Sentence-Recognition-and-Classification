f41e8a9a758480943a85b2f6bf51e3aec205c4df
GanquanWen <31709654+GanquanWen@users.noreply.github.com>
2018-12-03 17:42:32 -0500

    Segmentation upgrade
    New features:
    1. Variable min_gray added. It is for determined if a dot is white or black.
    2. Variable min_gap added. It is for more accurate segmentation. At least min_gap of blank columns(or rows) needs to be detected for the program to cut a character.
    3. Add a 'frame' around the segmented characters. The size of new segmentation window is now (n+8, m+8). The former one is (n, m). (4 px before the start point and 4 px behind the end point).

--- Sentence_Segmentation/seg_test.py
+++ Sentence_Segmentation/seg_test.py
@@ -1,5 +1,5 @@
 import segmentation
 
-img = segmentation.CharSeg("test_sample/", "test_sample/saved/", "bo_luo.jpg", (150, 150), 3, 0.7)
+img = segmentation.CharSeg("test_sample/", "test_sample/saved/", "0_0.png", output_size = (58, 58))
 print(img.image_gray.shape)
 img.run()
--- Sentence_Segmentation/segmentation.py
+++ Sentence_Segmentation/segmentation.py
@@ -4,17 +4,21 @@ import cv2
 import matplotlib.pyplot as plt
 
 class CharSeg():
-    def __init__ (self, dir_source, dir_saved, file_name, output_size = (300, 300), threshold = 2, size_flex = 0.75):
+    # All the input images are colored(RGB)
+    # if the image is already gray scale then the initialization needs to be adjusted
+    def __init__ (self, dir_source, dir_saved, file_name, output_size = (300, 300), min_gray = 200, threshold = 1, min_gap = 4, rows = 1, size_flex = 0.7):
         self.dir_source = dir_source # path to the source image folder (not including the file name)
         self.dir_saved = dir_saved # path to the folder to save the segmentaion
         self.file_name = file_name
         self.output_size = output_size # The output size of each chinense character
+        self.min_gray = min_gray
         self.threshold = threshold # To determine if this row(or column) contains parts of the characters
+        self.min_gap = min_gap # The minimum px between two character. Means at least min_gap of blank columns(or rows) needs to be detected for the program to cut a character
         self.size_flex = size_flex # One of the parameter to the characters cutting
         self.image = cv2.imread(self.dir_source + self.file_name)
         self.image_gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
 
-    def scan_image_horizontal(self, image):
+    def scan_image_horizontal(self, image, min_gray):
         # Scan the sentence column by column to count how many dot in one column
         # Save all the amount in an array "horizontal"
         height = image.shape[0]
@@ -23,22 +27,22 @@ class CharSeg():
         for x in range(width): # From left to right
             count = 0 # The amount of dots of each column
             for y in range(height): # From top to bottom
-                if (image[y][x] < 128) :
+                if (image[y][x] < min_gray) :
                     count = count + 1
             horizontal[x] = count
               # print(horizontal)
         return horizontal
 
-    def scan_image_vertical(self):
+    def scan_image_vertical(self, image, min_gray):
         # Scan the image row by row to count how many dot in one row
         # Save all the amount in an array "vertical"
-        height = self.image_gray.shape[0]
-        width = self.image_gray.shape[1]
+        height = image.shape[0]
+        width = image.shape[1]
         vertical = np.zeros(height, dtype = int)
         for y in range(height): # From top to bottom
             count = 0 # The amount of dots of each row
             for x in range(width): # From left to right
-                if (self.image_gray[y][x] < 128) :
+                if (image[y][x] < min_gray) :
                     count = count + 1
             vertical[y] = count
         # print(vertical)
@@ -57,49 +61,76 @@ class CharSeg():
                 return end_point - start_point
             else:
                 pass
-        return end_point - start_point
+        if end_point is None and start_point is not None:
+            end_point = len(vertical) - 1
+        return end_point - start_point + 1
 
-    def segmentation(self, dots_sum, threshold, min_size):
+    def segmentation(self, dots_sum, threshold, min_size, min_gap):
         # Cut the sentence into characters
         # Because chinese characters are basically rectangle,
         # So we can cut it according to the height of a sentence.
         start_i = None
         end_i = None
         parts = []
+        blank_count = 0
         for i, val in enumerate(dots_sum):
             if val > threshold and start_i is None:
-                start_i = i
+                if i > 4: # Add blank around the character
+                    start_i = i - 4
+                else:
+                    start_i = 0
             elif val > threshold and start_i is not None:
                 pass
             elif val < threshold and start_i is not None:
-                if i - start_i >= min_size:
-                    end_i = i
+                blank_count += 1
+                if (i - start_i >= min_size) and (blank_count >= min_gap):
+                    if i > len(dots_sum) - 6: # Add blank around the character
+                        end_i = len(dots_sum) - 1
+                    else:
+                        end_i = i + 4
                     # print(end_i - start_i)
                     parts.append((start_i, end_i))
                     start_i = None
                     end_i = None
+                    blank_count = 0
+                else:
+                    pass
             elif val < threshold and start_i is None:
                 pass
+
+        if (start_i is not None):
+            end_i = len(dots_sum) - 1
+            parts.append((start_i, end_i))
+
         return parts
 
-    def cutImage(self, img, vertical_ranges, char_height, output_size, dir_saved):
+    def cutImage(self, img, vertical_ranges, min_size, output_size, dir_saved):
+        results = []
         for y, vertical_index_pair in enumerate(vertical_ranges):
-            horizontal = self.scan_image_horizontal(img[vertical_index_pair[0] : vertical_index_pair[1], : ])
-            horizontal_ranges = self.segmentation(horizontal, 2, char_height * 0.75)
+            horizontal = self.scan_image_horizontal(img[vertical_index_pair[0] : vertical_index_pair[1], : ], self.min_gray)
+            horizontal_ranges = self.segmentation(horizontal, 2, min_size, self.min_gap)
             for x, horizontal_index_pair in enumerate(horizontal_ranges):
                 cutted_char = img[vertical_index_pair[0]    : vertical_index_pair[1], 
                                   horizontal_index_pair[0] : horizontal_index_pair[1]]
                 resized_cutted_char = cv2.resize(cutted_char, output_size)
+                results.append(resized_cutted_char)
                 cv2.imwrite(dir_saved + "row" + str(y) + "pos" + str(x) + ".jpg", resized_cutted_char)
                 print("row" + str(y) + "pos" + str(x) + "saved!")
+        return results
 
     def run(self):
-        vertical = self.scan_image_vertical()
+        vertical = self.scan_image_vertical(self.image_gray, self.min_gray)
         char_height = self.get_sentence_height(vertical)
-        print('The height of these characters is about: ' + str(char_height) + ' pixel')
-        vertical_segs = self.segmentation(vertical, self.threshold, char_height * self.size_flex)
+        print('The height of these characters is about: ' + str(char_height) + ' px')
+        vertical_segs = self.segmentation(vertical, self.threshold, char_height * self.size_flex, self.min_gap)
         print(vertical_segs)
-        self.cutImage(self.image_gray, vertical_segs, char_height, self.output_size, self.dir_saved)
+        result = self.cutImage(self.image_gray, vertical_segs, char_height * self.size_flex, self.output_size, self.dir_saved)
+        # print(result)
+        # print(len(result))
+        output = np.array(result)
+        print(output)
+        print(output.shape)
+        print(len(output))
 
 
 
